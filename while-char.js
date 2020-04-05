3ff5e9554fed95e9530bf16da5482e0ee422942b
Mirek Rusin <mirek@me.com>
2020-04-05 11:09:53 +0200

    Fixing and updating api for while-char.

--- while-char.js
+++ while-char.js
@@ -1,15 +1,21 @@
 // @flow
 
-/*:: import type { Parser as P } from './types/Parser' */
+const { inspect } = require('util')
+const Invalid = require('./invalid')
 
-const whileChar /*: (string => boolean) => P<string> */ =
-  f =>
+/*:: import type { Parser } from './types/parser' */
+
+const whileChar /*: (string, min?: number) => Parser<string> */ =
+  (chars, min = 0) =>
     input => {
       let i
       for (i = 0; i < input.length; i++) {
-        if (!f(input[0])) {
+        if (!chars.includes(input[i])) {
           break
         }
+      }
+      if (i < min) {
+        throw new Invalid(`Expected ${min} minimum characters passing predicate, character ${inspect(input[i])} didn't pass.`)
       }
       return [ input.slice(i), input.slice(0, i) ]
     }
