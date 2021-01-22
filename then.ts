209e5f57adc055542edecd04dabfe2b19d1130e1
Mirek Rusin <mirek@me.com>
2021-01-22 18:58:12 +0100

    Cosmetics.

--- then.ts
+++ then.ts
@@ -1,7 +1,7 @@
 import type P from './types/parser'
 
-export const then: <A, B>(a: P<A>, f: (_: A) => P<B>) => P<B> =
-  (a, f) =>
+export const then =
+  <A, B>(a: P<A>, f: (_: A) => P<B>): P<B> =>
     input => {
       const [ s, r ] = a(input)
       return f(r)(s)
